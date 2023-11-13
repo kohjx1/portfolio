463f3cabe59b991e63f4356434c218e0990d5dee
kohjx1 <kohjiaglenn@hotmail.com>
2023-11-13 22:25:56 +0800

    feat(parralax)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import styles from './styles.module.css'
+
 
 function App() {
+  const alignCenter = { display: "flex", alignItems: "center" };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className={styles.background} />
+
+      <Parallax pages={5}>
+        <ParallaxLayer
+          offset={0}
+          speed={0.5}
+          style={{ ...alignCenter, justifyContent: "center" }}
         >
-          Learn React
-        </a>
-      </header>
+          <p className={styles.scrollText}>Scroll down</p>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          sticky={{ start: 1, end: 4 }}
+          style={{ ...alignCenter, justifyContent: "flex-start" }}
+        >
+          <div className={`${styles.card} ${styles.sticky}`}>
+            <p>I'm a sticky layer</p>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={1.5}
+          speed={1.5}
+          style={{ ...alignCenter, justifyContent: "flex-end" }}
+        >
+          <div className={`${styles.card} ${styles.parallax} ${styles.purple}`}>
+            <p>I'm not</p>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2.5}
+          speed={1.5}
+          style={{ ...alignCenter, justifyContent: "flex-end" }}
+        >
+          <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
+            <p>Neither am I</p>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
