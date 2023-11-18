be6c0a40d0f2c764ea2c584c45f9e6b8a8d1c308
kohjx1 <kohjiaglenn@hotmail.com>
2023-11-18 23:05:48 +0800

    feat(parallax layer 1): added picture

--- src/App.js
+++ src/App.js
@@ -10,45 +10,44 @@ function App() {
   return (
     <div>
       {/* background */}
-      <div className="bg-gradient-to-t from-indigo-100 to-indigo-700 w-[100vw] h-[100vh] absolute" />
-      <div className="bg-transparent absolute h-[10vh] w-[100vw] flex justify-between text-gray-300 items-center">
-        <div className="w-[20vw] text-[1.5rem] grid justify-items-center">
+      <div className="bg-gradient-to-t from-black to-slate-800 w-[100svw] h-[100svh] absolute" />
+      <div className="bg-transparent absolute h-[10svh] w-[100svw] flex justify-between text-gray-300 items-center">
+        <div className="w-[20svw] text-[1.5rem] grid justify-items-center">
           Glenn Codes
         </div>
-        <div className="flex w-[80vw] text-[1.5rem] justify-end space-x-[3vw] mr-[5vw]">
+        <div className="w-[60svw] text-[1.5rem] grid justify-items-center">
+          <p className="text-[1.5rem] text-stone-400">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Hello There! Welcome to glenn's webpage :)")
+                  .pauseFor(1500)
+                  .start();
+              }}
+              options={{ loop: true, delay: 100, deleteSpeed: 50 }}
+            />
+          </p>
+        </div>
+        <div className="flex w-[20svw] text-[1.5rem] justify-end space-x-[3svw] mr-[5svw]">
           <div> Me</div>
           <div> Portfolio</div>
+          <div> Blog</div>
         </div>
       </div>
       <Parallax pages={5}>
         <ParallaxLayer
           offset={0}
           speed={0.5}
-          className="flex items-center justify-center"
         >
-          <p className={styles.scrollText}>
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString("Hello There! Welcome to glenn's webpage :)")
-                  .callFunction(() => {
-                    console.log("String typed out!");
-                  })
-                  .pauseFor(500)
-                  .start()
-                
-                  // .deleteAll()
-                  // .callFunction(() => {
-                  //   console.log("All strings were deleted");
-                  // })
-                  // .start();
-              }
-            }
-            options={
-              {loop:true}
-            }
-            />
-          </p>
+          <div className="flex ml-[5svw] mt-[30svh]">
+          <img
+            src="/profile-picture.jpg"
+            className="rounded-3xl w-[30svw] grayscale shadow-md flex justify-center"
+          />
+
+          <div className="w-[30svw] text-[1.5rem] text-stone-400 flex justify-center"> Test</div>
+          </div>
+          
         </ParallaxLayer>
 
         <ParallaxLayer
